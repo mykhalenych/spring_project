51f3b020a23bd8ae8241822c39c90b6af2884a70
mykhalenych <mykhalenych@gmail.com>
2026-06-26 12:19:16 +0300

    fixed comments after review

--- src/main/java/com/example/project/controller/BookController.java
+++ src/main/java/com/example/project/controller/BookController.java
@@ -5,16 +5,18 @@ import com.example.project.dto.CreateBookRequestDto;
 import com.example.project.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -29,6 +31,7 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
